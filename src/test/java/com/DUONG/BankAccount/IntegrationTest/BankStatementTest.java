c1acc8b2205fa146c56d3d1b817ac7a6c2d2d077
NoNemAgain <57526481+NoNemAgain@users.noreply.github.com>
2026-04-04 19:01:08 +0200

    Feat(BankStatementController): Add BankStatementController +  Test

---
+++ src/test/java/com/DUONG/BankAccount/IntegrationTest/BankStatementTest.java
@@ -0,0 +1,72 @@
+package com.DUONG.BankAccount.IntegrationTest;
+
+import com.DUONG.BankAccount.BankAccountFactory;
+import com.DUONG.BankAccount.adapter.out.repository.BankAccountRepository;
+import com.DUONG.BankAccount.adapter.out.repository.BankStatementRepository;
+import com.DUONG.BankAccount.domain.model.AccountType;
+import com.DUONG.BankAccount.domain.model.CheckingAccount;
+import com.DUONG.BankAccount.domain.service.bankStatement.service.BankStatementService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@ActiveProfiles("test")
+@AutoConfigureMockMvc
+public class BankStatementTest {
+
+    private MockMvc mockMvc;
+
+    private BankStatementService bankStatementService;
+
+    private BankAccountRepository bankAccountRepository;
+
+    private BankStatementRepository bankStatementRepository;
+
+    @BeforeEach
+    void setUp(WebApplicationContext context) {
+        mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
+
+        bankAccountRepository = context.getBean(BankAccountRepository.class);
+        bankStatementRepository = context.getBean(BankStatementRepository.class);
+
+        bankStatementService = new BankStatementService(bankStatementRepository, bankAccountRepository);
+
+    }
+
+    @Test
+    void test_generate_bank_statement_should_have_200() throws Exception {
+        //Given
+        CheckingAccount checkingAccount = (CheckingAccount) BankAccountFactory.bankAccountCreateTest(AccountType.CHECKING);
+
+        CheckingAccount saveCheckingAccount = bankAccountRepository.save(checkingAccount);
+
+        //WHEN //THEN
+        mockMvc.perform(post("/api/bankStatements/" + saveCheckingAccount.getId()))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.accountType").value(AccountType.CHECKING.toString()));
+
+    }
+
+    @Test
+    void test_generate_bank_statement_should_have_400() throws Exception {
+        //Given
+        CheckingAccount checkingAccount = (CheckingAccount) BankAccountFactory.bankAccountCreateTest(AccountType.CHECKING);
+
+        CheckingAccount saveCheckingAccount = bankAccountRepository.save(checkingAccount);
+
+        //WHEN //THEN
+        mockMvc.perform(post("/api/bankStatements/" + saveCheckingAccount.getId() + 1))
+                .andExpect(status().isBadRequest());
+
+    }
+}
