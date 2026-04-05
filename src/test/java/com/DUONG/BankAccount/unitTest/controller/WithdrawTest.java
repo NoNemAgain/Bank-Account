6a1e52df732418382e4c0361dd845d165e27f627
NoNemAgain <57526481+NoNemAgain@users.noreply.github.com>
2026-04-05 20:07:09 +0200

    Test(Controller) : Add controller unitTest

---
+++ src/test/java/com/DUONG/BankAccount/unitTest/controller/WithdrawTest.java
@@ -0,0 +1,64 @@
+package com.DUONG.BankAccount.unitTest.controller;
+
+import com.DUONG.BankAccount.BankAccountFactory;
+import com.DUONG.BankAccount.adapter.in.controller.BankAccountController;
+import com.DUONG.BankAccount.domain.model.AccountType;
+import com.DUONG.BankAccount.domain.model.CheckingAccount;
+import com.DUONG.BankAccount.domain.model.SavingAccount;
+import com.DUONG.BankAccount.port.in.DepositPort;
+import com.DUONG.BankAccount.port.in.WithdrawPort;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class WithdrawTest {
+
+    MockMvc mockMvc;
+
+    DepositPort depositPort;
+
+    WithdrawPort withdrawPort;
+
+    BankAccountController bankAccountController;
+
+    @BeforeEach
+    void setUp() {
+        depositPort = Mockito.mock(DepositPort.class);
+        withdrawPort = Mockito.mock(WithdrawPort.class);
+
+        bankAccountController = new BankAccountController(depositPort, withdrawPort);
+        mockMvc = MockMvcBuilders.standaloneSetup(bankAccountController).build();
+    }
+
+    @Test
+    void withdraw_ShouldHave200() throws Exception {
+        //GIVEN
+        CheckingAccount checkingAccount = (CheckingAccount) BankAccountFactory
+                .bankAccountCreateTest(AccountType.CHECKING);
+        checkingAccount.setId(UUID.randomUUID());
+        UUID id = checkingAccount.getId();
+
+        //WHEN
+        when(withdrawPort.withdraw(id, new BigDecimal("200")))
+                .thenReturn(checkingAccount);
+
+        //THEN
+
+        mockMvc.perform(patch("/api/bank-accounts/" + id + "/withdraw")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("200"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(checkingAccount.getId().toString()));
+    }
+}
