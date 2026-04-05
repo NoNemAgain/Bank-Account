2149b9187da31c7a4d8098261b0c7ffad7aa7e58
NoNemAgain <57526481+NoNemAgain@users.noreply.github.com>
2026-04-05 17:10:06 +0200

    Test(Withdraw + Deposit): add integration test

--- src/test/java/com/DUONG/BankAccount/IntegrationTest/DepositTest.java
+++ src/test/java/com/DUONG/BankAccount/IntegrationTest/DepositTest.java
@@ -1,111 +1,50 @@
 package com.DUONG.BankAccount.IntegrationTest;
 
-import com.DUONG.BankAccount.StrategyFactory;
+import com.DUONG.BankAccount.BankAccountFactory;
 import com.DUONG.BankAccount.adapter.out.repository.BankAccountRepository;
-import com.DUONG.BankAccount.adapter.out.repository.OperationRepository;
-import com.DUONG.BankAccount.domain.model.OperationType;
-import com.DUONG.BankAccount.domain.service.deposit.service.DepositService;
-import com.DUONG.BankAccount.domain.service.deposit.strategy.DepositStrategy;
+import com.DUONG.BankAccount.domain.model.AccountType;
+import com.DUONG.BankAccount.domain.model.CheckingAccount;
+import com.DUONG.BankAccount.domain.model.SavingAccount;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import java.util.List;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@ActiveProfiles("test")
 @AutoConfigureMockMvc
 public class DepositTest {
 
-    MockMvc mockMvc;
+    private MockMvc mockMvc;
 
-    DepositService depositService;
-
-    OperationRepository operationRepository;
-
-    BankAccountRepository bankAccountRepository;
-
-    List<DepositStrategy> strategyList;
+    private BankAccountRepository bankAccountRepository;
 
     @BeforeEach
     void setUp(WebApplicationContext context) {
         mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
-        operationRepository = context.getBean(OperationRepository.class);
         bankAccountRepository = context.getBean(BankAccountRepository.class);
-        strategyList = StrategyFactory.strategyListCreate(OperationType.DEPOSIT);
-
-        depositService = new DepositService(bankAccountRepository, operationRepository, strategyList);
-
-
-    }
-    /*
-    @Autowired
-    private MockMvc mockMvc;
-
-    @Autowired
-    private CheckingAccountService checkingAccountService;
-
-    @Autowired
-    private CheckingAccountRepository checkingAccountRepository;
-
-    @Test
-    void withdraw_should_have_200() throws Exception {
-        //Given
-        CheckingAccount checkingAccount = new CheckingAccount();
-        checkingAccount.setBalance(1000);
-        CheckingAccount saveCheckingAccount = checkingAccountRepository.save(checkingAccount);
-
-        //WHEN //THEN
-        mockMvc.perform(post("/api/checkingAccount/withdraw/" + saveCheckingAccount.getId())
-                        .param("amount", "200"))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.balance").value("800.0"));
-    }
-
-    @Test
-    void withdraw_negative_amount_should_have_400() throws Exception {
-        //Given
-        CheckingAccount checkingAccount = new CheckingAccount();
-        checkingAccount.setBalance(1000);
-        CheckingAccount saveCheckingAccount = checkingAccountRepository.save(checkingAccount);
-
-        //WHEN //THEN
-        mockMvc.perform(post("/api/checkingAccount/withdraw/" + saveCheckingAccount.getId())
-                        .param("amount", "-200"))
-                .andExpect(status().isBadRequest())
-                .andExpect(jsonPath("$.message").value("Le montant à retirer doit être positif."));
-    }
-
-    @Test
-    void withdraw_exceed_limit_amount_should_have_400() throws Exception {
-        //Given
-        CheckingAccount checkingAccount = new CheckingAccount();
-        checkingAccount.setBalance(1000);
-        checkingAccount.setExceedAllowed(true);
-        checkingAccount.setExceedBalance(400.0);
-        CheckingAccount saveCheckingAccount = checkingAccountRepository.save(checkingAccount);
-
-        //WHEN //THEN
-        mockMvc.perform(post("/api/checkingAccount/withdraw/" + saveCheckingAccount.getId())
-                        .param("amount", "2000"))
-                .andExpect(status().isBadRequest())
-                .andExpect(jsonPath("$.message").value("Vous n’avez pas assez d’argent sur votre compte : vous ne pouvez pas retirer cet argent."));
     }
 
     @Test
     void deposit_should_have_200() throws Exception {
         //GIVEN
-        CheckingAccount checkingAccount = new CheckingAccount();
-        checkingAccount.setBalance(1000);
-
-        CheckingAccount saveCheckingAccount = checkingAccountRepository.save(checkingAccount);
+        CheckingAccount checkingAccount = (CheckingAccount) BankAccountFactory.bankAccountCreateTest(AccountType.CHECKING);
+        CheckingAccount saveCheckingAccount = bankAccountRepository.save(checkingAccount);
 
 
         //WHEN && THEN
-        mockMvc.perform(post("/api/checkingAccount/deposit/" + saveCheckingAccount.getId())
-                        .param("amount", "200"))
+        mockMvc.perform(patch("/api/bank-accounts/" + saveCheckingAccount.getId() + "/deposit")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("200"))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.balance").value("1200.0"));
     }
@@ -113,18 +52,14 @@ public class DepositTest {
     @Test
     void deposit_negative_amount_should_have_400() throws Exception {
         //GIVEN
-        CheckingAccount checkingAccount = new CheckingAccount();
-        checkingAccount.setBalance(1000);
-
-        CheckingAccount saveCheckingAccount = checkingAccountRepository.save(checkingAccount);
-
+        SavingAccount savingAccount = (SavingAccount) BankAccountFactory.bankAccountCreateTest(AccountType.SAVING);
+        SavingAccount saveSavingAccount = bankAccountRepository.save(savingAccount);
 
         //WHEN && THEN
-        mockMvc.perform(post("/api/checkingAccount/deposit/" + saveCheckingAccount.getId())
-                        .param("amount", "-200"))
+        mockMvc.perform(patch("/api/bank-accounts/" + saveSavingAccount.getId() + "/deposit")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("-200"))
                 .andExpect(status().isBadRequest())
-                .andExpect(jsonPath("$.message").value("Le montant à déposer doit être positif."));
+                .andExpect(jsonPath("$.message").value("The amount must be positive"));
     }
-    */
-
 }
